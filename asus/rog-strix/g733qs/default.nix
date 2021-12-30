9b2f97839f7b3c5923e2135e34a9fdd59aef9dbd
Pasquale <p3dimaria@hotmail.it>
2021-12-30 21:39:42 +0100

    jack-retask not needed anymore

--- asus/rog-strix/g733qs/default.nix
+++ asus/rog-strix/g733qs/default.nix
@@ -10,14 +10,14 @@
 
   # fixing audio by overriding pins as suggested in
   # https://www.reddit.com/r/ASUS/comments/mfokva/asus_strix_scar_17_g733qs_and_linux/
-  hardware.firmware = [
-    (pkgs.runCommand "jack-retask" { } ''
-      install -D ${./hda-jack-retask.fw} $out/lib/firmware/hda-jack-retask.fw
-    '')
-  ];
-  boot.extraModprobeConfig = ''
-    options snd-hda-intel patch=hda-jack-retask.fw
-  '';
+  # hardware.firmware = [
+  #   (pkgs.runCommand "jack-retask" { } ''
+  #     install -D ${./hda-jack-retask.fw} $out/lib/firmware/hda-jack-retask.fw
+  #   '')
+  # ];
+  # boot.extraModprobeConfig = ''
+  #   options snd-hda-intel patch=hda-jack-retask.fw
+  # '';
   # before 5.12 it would interpret every keystroke as the power button
   boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "5.12") (lib.mkDefault pkgs.linuxPackages_latest);
 
